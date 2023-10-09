9758000c14c117cd77eabb4b32b2ad5ff534a92c
ribas858 <ribas858@gmail.com>
2023-10-09 18:49:17 -0300

    modo 400 e 444

--- proj.m
+++ proj.m
@@ -2,33 +2,52 @@ clear all;
 close all;
 clc;
 
-video = fopen("6p4p_422.yuv", "rb");
-modo = 422
+video = fopen("6p4p_444.yuv", "rb");
+modo = 444;
+frame = 10;
 w = 6;
 h = 4;
 
-% tam_f = w * h * 1.5;
-
-% pos_f = tam_f * (10 - 1);
-
-% fseek(video, pos_f, 'bof');
-
-Y = fread(video, w * h, 'uchar');
 
 if modo == 420
+  tam_f = w * h * 1.5;
+  posicao_f = tam_f * frame;
+  fseek(video, posicao_f, 'bof');
+  Y = fread(video, w * h, 'uchar');
+  
   horizontal = 2;
   vertical = 2;
   U = fread(video, (w/2) * (h/2), 'uchar');
   V = fread(video, (w/2) * (h/2), 'uchar');
+  
 elseif modo == 422
+  tam_f = w * h * 2;
+  posicao_f = tam_f * frame;
+  fseek(video, posicao_f, 'bof');
+  Y = fread(video, w * h, 'uchar');
+  
   horizontal = 2;
   vertical = 1;
   U = fread(video, (w/2) * h, 'uchar');
   V = fread(video, (w/2) * h, 'uchar');
+
 elseif modo == 444
+  tam_f = w * h * 3;
+  posicao_f = tam_f * frame;
+  fseek(video, posicao_f, 'bof');
+  Y = fread(video, w * h, 'uchar');
+  
   horizontal = 1;
   vertical = 1;
+  U = fread(video, w * h, 'uchar');
+  V = fread(video, w * h, 'uchar');
+  
 elseif modo == 400
+  tam_f = w * h;
+  posicao_f = tam_f * frame;
+  fseek(video, posicao_f, 'bof');
+  Y = fread(video, w * h, 'uchar');
+
 end
 
 imagem = zeros(h, w, 3);
@@ -45,40 +64,53 @@ for i = 1 : size(imagem, 1)
     for j = 1 : size(imagem, 2)
         
         Y_ =  Y(k_y) - 16;
-        U_ =  U(k_u) - 128;
-        V_ =  V(k_v) - 128;
         
-        R = (0.25344 * Y_ - 0.00001 * U_ + 0.291878 * V_) / 0.25344;
-        G = (0.25344 * Y_ - 0.10001 * U_ - 0.147122 * V_) / 0.25344;
-        B = (0.25344 * Y_ + 0.51499 * U_ - 0.000122 * V_) / 0.25344;
+        if modo == 400
+          R = Y_;
+          G = Y_;
+          B = Y_;
+        else
+          U_ =  U(k_u) - 128;
+          V_ =  V(k_v) - 128;
+          
+          R = (0.25344 * Y_ - 0.00001 * U_ + 0.291878 * V_) / 0.25344;
+          G = (0.25344 * Y_ - 0.10001 * U_ - 0.147122 * V_) / 0.25344;
+          B = (0.25344 * Y_ + 0.51499 * U_ - 0.000122 * V_) / 0.25344;
+        end
         
         imagem(i, j, 1) = R / 255;
         imagem(i, j, 2) = G / 255;
         imagem(i, j, 3) = B / 255;
         
-        intervalo_j = intervalo_j + 1;
-        
         k_y = k_y + 1;
-        if intervalo_j == horizontal
-          intervalo_j = 0;
-          if k_u < numel(U) && k_v < numel(V)  
-            k_u =  k_u + 1;
-            k_v =  k_v + 1;
-          end
-          fprintf('\n');
-        end
         
-        disp(['Comp U: ', num2str(k_u), ' Comp V: ', num2str(k_v)]);
+        if modo != 400
+          intervalo_j = intervalo_j + 1;
+  
+          if intervalo_j == horizontal
+            intervalo_j = 0;
+            if k_u < numel(U) && k_v < numel(V)  
+              k_u =  k_u + 1;
+              k_v =  k_v + 1;
+            end
+            fprintf('\n');
+          end
+          
+          disp(['Comp U: ', num2str(k_u), ' Comp V: ', num2str(k_v)]);
+        endif
     end
     
-    intervalo_i = intervalo_i + 1;
-    if intervalo_i == vertical
-      origem_UV = origem_UV + w/2;
-      intervalo_i = 0;
-    else
-      k_u = origem_UV;
-      k_v = origem_UV;
-    end
+    if modo != 400
+      intervalo_i = intervalo_i + 1;
+      if intervalo_i == vertical
+        origem_UV = origem_UV + w/2;
+        intervalo_i = 0;
+      else
+        k_u = origem_UV;
+        k_v = origem_UV;
+      end
+    endif
+    
 end
 disp('FIM: ');
 disp(['Origem_UV: ', num2str(origem_UV)]);
